cada22fec3935009992b76862534fcbe29aca255
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2021-03-10 18:27:13 -0300

    Release v0.1.1
    * Update url.ts
    
    * Automatic update on Toolbelt plugin documentation
    
    * Remove collor script
    
    * Automatic update on Toolbelt plugin documentation
    
    * CHANGELOG
    
    * Release 0.1.0
    
    * Release v0.1.1
    
    * Automatic update on Toolbelt plugin documentation
    
    Co-authored-by: Thiago Veras <thiago.veras.machado@gmail.com>

--- src/commands/url.ts
+++ src/commands/url.ts
@@ -1,10 +1,12 @@
-import { CustomCommand } from 'vtex'
+import { CustomCommand, ColorifyConstants } from 'vtex'
 import authUrl from '../modules/url'
 
 export default class URL extends CustomCommand {
-  static description = 'Prints base URL for current account, workspace and environment'
+	static description = `Prints the base URL for the current ${ColorifyConstants.ID(
+		'account'
+	)}, ${ColorifyConstants.ID('workspace')} and ${ColorifyConstants.ID('environment')}`;
 
-  static examples = ['vtex url']
+	static examples = [ `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex url')}` ];
 
   static flags = {
     ...CustomCommand.globalFlags,
