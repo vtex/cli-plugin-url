ec544b8feb44dd9c6cabbcc017cbd0ea62750ec0
Thiago Veras <thiago.veras.machado@gmail.com>
2021-03-30 15:45:16 -0300

    Add lint files

--- src/commands/url.ts
+++ src/commands/url.ts
@@ -2,11 +2,11 @@ import { CustomCommand, ColorifyConstants } from 'vtex'
 import authUrl from '../modules/url'
 
 export default class URL extends CustomCommand {
-	static description = `Prints the base URL for the current ${ColorifyConstants.ID(
-		'account'
-	)}, ${ColorifyConstants.ID('workspace')} and ${ColorifyConstants.ID('environment')}`;
+  static description = `Prints the base URL for the current ${ColorifyConstants.ID('account')}, ${ColorifyConstants.ID(
+    'workspace'
+  )} and ${ColorifyConstants.ID('environment')}`
 
-	static examples = [ `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex url')}` ];
+  static examples = [`${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex url')}`]
 
   static flags = {
     ...CustomCommand.globalFlags,
