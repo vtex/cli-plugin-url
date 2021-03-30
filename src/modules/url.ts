7490f99ba5c01315138788a17afabbac05a43fac
Thiago Veras <thiago.veras.machado@gmail.com>
2021-03-30 17:45:59 -0300

    Release v0.1.2 (#3)
    * Add lint files
    
    * CHANGELOG
    
    * Automatic update on Toolbelt plugin documentation
    
    * CHANGELOG
    
    * Release v0.1.2
    
    * Automatic update on Toolbelt plugin documentation
    
    Co-authored-by: João Marcos Gris <joao.gris@vtex.com.br>

--- src/modules/url.ts
+++ src/modules/url.ts
@@ -2,5 +2,6 @@ import { clusterIdDomainInfix, publicEndpoint, SessionManager } from 'vtex'
 
 export default () => {
   const { account, workspace } = SessionManager.getSingleton()
+
   return `https://${workspace}--${account}${clusterIdDomainInfix()}.${publicEndpoint()}`
 }
